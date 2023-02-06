e3e45bd745295e4e5c84d842d8a2abfd71060bfa
poow810 <poow810@naver.com>
2023-02-06 09:39:29 +0900

    day10 toString override

---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    public String data;
+    public Node link;   // 자기 자신의 주소를 받아야함 : Node
+
+    @Override
+    public String toString() {      // 최상위 클래스인 object를 오버라이드
+        return "노드의 데이터 값은 " + data + "입니다.";
+    }
+}
