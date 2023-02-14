bc243adc7cf42fc4ca559a40aa2f9c072a06e382
poow810 <poow810@naver.com>
2023-02-14 17:58:01 +0900

    day22_homework-3

--- day22/Day22_homework.java
+++ day22/Day22_homework.java
@@ -1,9 +1,13 @@
 package day22;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Day22_homework {
     public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
         int alphabet = 'a';
-        Thread t = new Thread(() -> {
+        Runnable task = () -> {
             try {
                 for (int i = 0; i < 5; i++) {
                     System.out.println("작업 스레드 : " + i);
@@ -12,8 +16,7 @@ public class Day22_homework {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        });
-        t.start();
+        };exec.execute(task);
         try {
             for (alphabet = 97; alphabet < 107; alphabet += 1) {
                 System.out.println("메인 스레드 : " + (char) alphabet);
@@ -21,6 +24,6 @@ public class Day22_homework {
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }
+        }exec.shutdown();
     }
 }
