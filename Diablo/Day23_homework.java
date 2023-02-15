5ad4a7ec8e2051768f432e7aa8c6f87a4e74587d
poow810 <poow810@naver.com>
2023-02-15 20:56:47 +0900

    Day23_homework_3

--- Diablo/Day23_homework.java
+++ Diablo/Day23_homework.java
@@ -1,6 +1,8 @@
 package Diablo;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -8,23 +10,75 @@ enum Gender{
     남, 여;
 }
 
+class Member{
+    String name;
+    Gender gender;
+    int age;
+
+    public Member(String name, Gender gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public String toString(){
+        return String.format("Member(%s, %s, %d)", name, gender, age);
+    }
+}
+
 
 public class Day23_homework {
+    static int i = 0;
     public static void main(String[] args) {
-
+        List<String> names = List.of("홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이");
         List<Integer> ages = List.of(25, 20, 29, 28, 32, 18);
         List<Gender> genders = List.of(Gender.남, Gender.여, Gender.남, Gender.남, Gender.남, Gender.여);
 
-        Stream<Integer> stream = ages.stream();
-        System.out.println(stream.reduce(0, Integer::sum));
-        stream = ages.stream();
-        System.out.println(stream.max(Integer::compareTo).get());
-        stream = ages.stream();
+        Stream<String> stream = names.stream();
+        stream.filter(n -> n.charAt(0) < '이').forEach(n -> System.out.print(n+" "));
+        System.out.println();
+
+        stream = names.stream();
+        stream.sorted().forEach(n -> System.out.print(n+ " "));
+        System.out.println();
+
+        stream = names.stream();
+        System.out.println(names.stream().findFirst());
+        System.out.println(names.stream().findFirst().get());
+        System.out.println(stream.count());
+
+        Stream<Integer> streams = ages.stream();
+        System.out.println(streams.reduce(0, Integer::sum));
+        streams = ages.stream();
+        System.out.println(streams.max(Integer::compareTo).get());
+        streams = ages.stream();
         System.out.println(
                 ages.stream()
                         .mapToInt(n->n.intValue())
                         .average()
                         .getAsDouble()
         );
+
+        stream = names.stream();
+        Stream<Member> sm = stream.map(n -> new Member(n, genders.get(i), ages.get(i++)));
+        sm.forEach(m -> System.out.print(m + " "));
+        System.out.println();
+
+        i = 0;
+        stream = names.stream();
+        sm = stream.map(n -> new Member(n, genders.get(i), ages.get(i++)));
+        Map<Gender, List<Member>> map = sm.collect(Collectors.groupingBy(Member::getGender));
+        System.out.println(map);
     }
-}
+    }
